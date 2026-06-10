41c4e8b324e78b0da22e1408e5b9f66b02ff6520
nikonikoni4 <1553542270@qq.com>
2026-06-10 09:30:03 +0800

    feat:补充之前漏提交的上传文件，前端部分

--- frontend/src/shared/hooks/index.ts
+++ frontend/src/shared/hooks/index.ts
@@ -1,4 +1,8 @@
 export { useWebSocketConnection } from './useWebSocketConnection';
 export { useGroupChatMembers, type MemberWithRole } from './useGroupChatMembers';
 export { useCreateChatData } from './useCreateChatData';
-export { useFileUpload, type UseFileUploadOptions, type UseFileUploadReturn } from './useFileUpload';
+export {
+  useFileUpload,
+  type UseFileUploadOptions,
+  type UseFileUploadReturn,
+} from './useFileUpload';
